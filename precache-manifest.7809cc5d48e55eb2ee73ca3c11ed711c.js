49b348ad93b0e4ba39a34fe2ffdf871c5f494b38
samhayder <sams.seul@gmail.com>
2020-04-14 14:07:11 +0600

    Updates

---
+++ precache-manifest.7809cc5d48e55eb2ee73ca3c11ed711c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997b177f34454900fc824abf92662e3d",
+    "url": "/thevenue/index.html"
+  },
+  {
+    "revision": "0f9122ad5dd0d8a34994",
+    "url": "/thevenue/static/css/main.af5e63ea.chunk.css"
+  },
+  {
+    "revision": "72995c13163d4f308f36",
+    "url": "/thevenue/static/js/2.439b7894.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/thevenue/static/js/2.439b7894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f9122ad5dd0d8a34994",
+    "url": "/thevenue/static/js/main.5be358f8.chunk.js"
+  },
+  {
+    "revision": "61b62ce8047ba2e51b17",
+    "url": "/thevenue/static/js/runtime-main.d7f7212b.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/thevenue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/thevenue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/thevenue/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
